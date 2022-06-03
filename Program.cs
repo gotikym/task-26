bca791b3caaf5844631e0fa4e2f83663f5aee92e
gotikym <gotikym9@mail.ru>
2022-06-03 11:12:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 11, 12, 13, 22, 99, 64, 31, 16, 77, 41, 54, 16, 17, 14, 43, 54, 16, 88, 65 };
+        int temporaryNumber = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    temporaryNumber = array[i];
+                    array[i] = array[j];
+                    array[j] = temporaryNumber;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
